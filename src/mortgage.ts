7d3b82ef34df2b79c7feb1747817cabff7aa31d3
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-08-25 18:57:16 -0300

    refactor: calculate periodic payment based on `monthly` (#8)

--- src/mortgage.ts
+++ src/mortgage.ts
@@ -1,8 +1,4 @@
-import { 
-  calcEffectiveInterestRate,
-  calcPeriodicInterestRate, 
-  resolvePaymentRate
-} from "./interestRate";
+import { calcEffectiveRate, calcMonthlyRate } from "./interestRate";
 import { validate } from "./validator";
 
 export function calculate(
@@ -17,46 +13,46 @@ export function calculate(
   if (errors.length > 0) return errors;
   
   const compoundingFrequency = 2;
-  const principalLoanAmount = propertyPrice - downPayment;
-  const numberOfPayments = paymentSchedule === "bi-weekly" ? amortization * 26 : amortization * 12;
+  const principal = propertyPrice - downPayment;
 
-  const paymentRate = resolvePaymentRate(paymentSchedule);
-  const effectiveInterestRate = calcEffectiveInterestRate(interestRate, compoundingFrequency);
-  const periodicInterestRate = calcPeriodicInterestRate(effectiveInterestRate, paymentRate);
-
-  const paymentPerSchedule = paymentSchedule === "accelerated-bi-weekly" 
-    ? calcPaymentPerSchedule(periodicInterestRate, numberOfPayments, principalLoanAmount) / 2
-    : calcPaymentPerSchedule(periodicInterestRate, numberOfPayments, principalLoanAmount)
+  const effectiveRate = calcEffectiveRate(interestRate, compoundingFrequency);
+  const monthlyRate = calcMonthlyRate(effectiveRate);
+  const monthlyPayment = calcMonthlyPayment(monthlyRate, principal, amortization)
+  const paymentPerSchedule = calcPaymentPerSchedule(paymentSchedule, monthlyPayment);
   
   const paymentPerScheduleFormatted = Number.parseFloat(paymentPerSchedule.toFixed(2));
   return paymentPerScheduleFormatted;
 }
 
-export function calcPaymentPerSchedule(periodicInterestRate: number, numberOfPayments: number, principalLoanAmount: number): number {
+function calcMonthlyPayment(monthlyRate: number, principalLoanAmount: number, amortization: number): number {
     // Formula from: https://www.bankrate.com/mortgages/mortgage-calculator/#how-mortgage-calculator-help
-    const numerator = periodicInterestRate * Math.pow(1 + periodicInterestRate, numberOfPayments);
-    const denominator = Math.pow(1 + periodicInterestRate, numberOfPayments) - 1;
-    const paymentPerSchedule = (principalLoanAmount * numerator) / denominator;
-
-    return paymentPerSchedule;
+    const amortizationInMonths = amortization * 12;
+    const numerator = monthlyRate * principalLoanAmount
+    const denominator = 1 - Math.pow(1 + monthlyRate, -amortizationInMonths);
+    const monthlyPayment = numerator / denominator;
+    return monthlyPayment;
 }
+
+function calcPaymentPerSchedule(paymentSchedule: string, monthlyPayment: number): number {
+  // Annualize the payments and divide it by the number of payments per year
+  if (paymentSchedule === "bi-weekly") return (monthlyPayment * 12) / 26;
+  if (paymentSchedule === "monthly") return monthlyPayment;
+  if (paymentSchedule === "accelerated-bi-weekly") return monthlyPayment / 2;
+  throw new Error("Invalid payment schedule");
+} 
 
 export function calcMinimumDownPayment(propertyPrice: number): number {
   // Data from: https://www.canada.ca/en/financial-consumer-agency/services/mortgages/down-payment.html
-  let minimumDownPayment = 0;
-
   // 5% of the purchase price
   if (propertyPrice <= 500000) 
-    minimumDownPayment = propertyPrice * 0.05;
+    return propertyPrice * 0.05;
 
   // 5% of the first $500,000 of the purchase price
   // 10% for the portion of the purchase price above $500,000
   else if (propertyPrice > 500000 && propertyPrice <= 999999) 
-    minimumDownPayment = 25000 + ((propertyPrice - 500000) * 0.1);
+    return  25000 + ((propertyPrice - 500000) * 0.1);
 
   // 20% of the purchase price
   else 
-    minimumDownPayment = propertyPrice * 0.2;
-  
-  return minimumDownPayment;
+    return  propertyPrice * 0.2;
 }
